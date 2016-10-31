07652b61a1c8e5ad438b8a6b32a7957423b83c4d
himynameisdave <d@velunny.com>
2016-10-30 20:25:23 -0700

    WIP - starts to check decl.value for any quote pairs
    WIP - starts to check decl.value for any quote pairs, using a
    .split().reduce fn that is currently broken and always returning 0 as
    the count of quotes, even tho quotes exist.
    
    More work to be done...

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
-var postcss = require('postcss');
+const postcss = require('postcss');
 
-module.exports = postcss.plugin('postcss-quotes', function (opts) {
-    opts = opts || {};
 
-    // Work with options here
+//  Determine if a given string contains pair of matching quotes
+const checkQuotes = (str) => {
+  console.log(str);
+  // TODO: ensure that there are a PAIR of quotes...
+  //    ...otherwise `url("I'm a URL")` would pass as a 'single'
+  if (str.indexOf('\'') > -1) {
+    //  TODO: break off into own fn
+    const singleQuoteCount = str.split().reduce((acc, item) => {
+      if (item === '\'') {
+        acc = acc + 1;
+      }
+      return acc;
+    }, 0);
+    console.log('The singleQuoteCount is: ' + singleQuoteCount);
+    return 'single';
+  }
+  if (str.indexOf('\"') > -1) {
+    const doubleQuoteCount = str.split().reduce((acc, item) => {
+      if (item === '\"') {
+        acc = acc + 1;
+      }
+      return acc;
+    }, 0);
+    console.log('The doubleQuoteCount is: ' + doubleQuoteCount);
+    return 'double';
+  }
+  return false;
+};
 
-    return function (css) {
 
-        // Transform CSS AST here
 
+module.exports = postcss.plugin('postcss-quotes', (opts) => {
+    //  Default to single-style quotes
+    const quoteType = opts && opts.quotes ? opts.quotes : 'single';
+
+
+    return (css) => {
+      css.walkRules(rule => {
+        rule.walkDecls((decl, i) => {
+          //  Our cleaned declaration value
+          let cleanDecl = decl.value;
+          const hasQuotes = checkQuotes(cleanDecl);
+          //  If there are quotes, lets run our
+          // if (hasQuotes) {
+          //   if (quoteType === hasQuotes) {
+          //     return
+          //   }
+          // }
+          //  If there are no quotes, just return the decl.value
+          return cleanDecl;
+        });
+      });
     };
 });
