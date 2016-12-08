f2302863e21e7fe41690ea29a43cb8890e8bf42d
himynameisdave <d@velunny.com>
2016-12-07 21:41:00 -0800

    Fixes/finishes first 5 test in test suite
    Fixes/finishes first five parts of test suite

---
+++ test/helpers/tests.js
@@ -0,0 +1,20 @@
+const tests = [
+  {
+    message: '01-charset',
+    fixturePath: 'fixtures/01-charset/fixtures.01-charset'
+  }, {
+    message: '02-single-set',
+    fixturePath: 'fixtures/02-single-set/fixtures.02-single-set'
+  }, {
+    message: '03-double-set',
+    fixturePath: 'fixtures/03-double-set/fixtures.03-double-set'
+  }, {
+    message: '04-mixed',
+    fixturePath: 'fixtures/04-mixed/fixtures.04-mixed'
+  }, {
+    message: '05-escaped',
+    fixturePath: 'fixtures/05-escaped/fixtures.05-escaped'
+  }
+];
+
+module.exports = tests;
--- test/test.postcss.js
+++ test/test.postcss.js
@@ -1,37 +1,27 @@
 import test from 'ava';// eslint-disable-line
-const fs = require('fs');
+const { readFileSync } = require('fs');
+const { resolve } = require('path');
 const postcss = require('postcss');
 const plugin = require('../');
+const tests = require('./helpers/tests.js');
 
 
-const tests = [
-  {
-    message: '02-single-set',
-    fixturePath: '02-single-set/fixtures.02-single-set'
-  }, {
-    message: '03-double-set',
-    fixturePath: '03-double-set/fixtures.03-double-set'
-  }, {
-    message: '04-mixed',
-    fixturePath: '04-mixed/fixtures.04-mixed'
-  }
-];
+//  Processes the CSS
+const doPostcss = (quotes) => (actual) => postcss(plugin({ quotes })).process(actual);
+//  Actually compares the css
+const doCompare = (quotes) => (t, fixturePath) => ({ css }) => {
+  const expected = readFileSync(`./test/${fixturePath}.expected_${quotes}.css`, 'utf-8');
+  t.is(css, expected);
+};
+//  Run a test
+const doTest = (quote, actual) => (t, fixturePath) => doPostcss(quote)(actual).then(doCompare(quote)(t, fixturePath));
 
 tests.map(v => { // eslint-disable-line array-callback-return
-  const actual = fs.readFileSync(`./test/fixtures/${v.fixturePath}.actual.css`, 'utf-8');
-  //  Do singles test
-  test(`${v.message} => 'single'`, t => {
-    return postcss(plugin({ quotes: 'single' })).process(actual).then(({ css }) => {
-      const expected = fs.readFileSync(`./test/fixtures/${v.fixturePath}.expected_single.css`, 'utf-8');
-      // console.log('HERES THE CSS: \n\n\n\n\n', css === expected)
-      t.is(css, expected);
-    });
-  });
-  //  Do doubles test
-  test(`${v.message} => 'double'`, t => {
-    return postcss(plugin({ quotes: 'double' })).process(actual).then(({ css }) => {
-      const expected = fs.readFileSync(`./test/fixtures/${v.fixturePath}.expected_double.css`, 'utf-8');
-      t.is(css, expected);
-    });
-  });
+  const actual = readFileSync(resolve(__dirname, `${v.fixturePath}.actual.css`), 'utf-8');
+  //  Test single
+  const single = doTest('single', actual);
+  test(`${v.message} => 'single'`, t => single(t, v.fixturePath));
+  //  Test double
+  const double = doTest('double', actual);
+  test(`${v.message} => 'double'`, t => double(t, v.fixturePath));
 });
